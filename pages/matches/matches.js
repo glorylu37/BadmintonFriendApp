111f550957c78b4bd1e4a1fab6d3dadbe1bc7cb8
glorylu37 <glorylu37@gmail.com>
2026-09-12 14:46:50 +0800

    feat: add pages/matches/matches.js

---
+++ pages/matches/matches.js
@@ -0,0 +1,8 @@
+Page({
+  data: {
+    matches: [
+      { title: "周六晚双打", place: "学校体育馆", level: "入门至初级", time: "19:00 - 21:00" },
+      { title: "周日晨练", place: "大学城羽毛球馆", level: "初级", time: "09:00 - 11:00" }
+    ]
+  }
+})
